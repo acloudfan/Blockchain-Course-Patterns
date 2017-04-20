645c23d8a85007cdf9a2fe74ad6bc99c92a5b800
rajrin@yahoo.com <raj@acloudfan.com>
2017-04-19 21:24:22 -0400

    done

--- contracts/NameRegistry.sol
+++ contracts/NameRegistry.sol
@@ -8,7 +8,7 @@ contract NameRegistry {
   // Manages info about the contract instance
   struct ContractInfo {
     address   owner;
-    address   contractInstance;
+    address   contractInst;
     // The first version added to registry MUST be >= 1
     // Otherwise the name will NOT be added
     uint16    version;
@@ -23,20 +23,21 @@ contract NameRegistry {
     // Version MUST start with number 1
     if(ver < 1) throw;
 
-    ContractInfo  conInfo = nameInfo[name];
-    if(conInfo.contractInstance == 0){
-      nameInfo[name] = ContractInfo(msg.sender, conAddress, ver);
+    if(nameInfo[name].contractInst == 0){
+      nameInfo[name].owner=msg.sender;
+      nameInfo[name].contractInst = conAddress;
+      nameInfo[name].version = ver;
     } else {
-      if(conInfo.owner != msg.sender)  return false;
-      conInfo.contractInstance = this;//conAddress;
-      conInfo.version = ver;
+      if(nameInfo[name].owner != msg.sender)  return false;
+      nameInfo[name].contractInst = conAddress;
+      nameInfo[name].version = ver;
     }
     return true;
   }
 
   // Contracts having a dependency on this contract will invoke this function
   function  getContractInfo(bytes32 name) constant returns(address,uint16){
-    return (nameInfo[name].contractInstance, nameInfo[name].version);
+    return (nameInfo[name].contractInst, nameInfo[name].version);
   }
 
   function  removeContract() returns(bool){
--- exec/NameRegistry.js
+++ exec/NameRegistry.js
@@ -8,15 +8,18 @@ module.exports = function (callback) {
     var nameRegistry;
     NameRegistry.deployed().then(function (instance) {
         nameRegistry = instance;
-        //1. Add the name & contract address
+        
+        //1. Add version 1 of contracts
         nameRegistry.registerName ("CheckingAccountFactory",generateRandomAddress(), 1);
         nameRegistry.registerName ("SavingAccountFactory", generateRandomAddress(), 1);
 
+        //2. Print the names & information on console
         PrintRegistry(nameRegistry);
 
-        //3. Get the value
+        //3. Update to version 2 - update the address for the contract
         return nameRegistry.registerName("CheckingAccountFactory", generateRandomAddress(), 2);
     }).then(function(result){
+        //4. Print the names & information on console
         PrintRegistry(nameRegistry);
     });
 }
@@ -26,6 +29,7 @@ function PrintRegistry(nameRegistry) {
         console.log("CheckingAccountFactory", result[0],' version=', result[1].toNumber());
         return nameRegistry.getContractInfo("SavingAccountFactory")
     }).then(function(result){
+        // console.log(result)
         console.log("SavingAccountFactory", result[0],' version=', result[1].toNumber());
         console.log('------------------------------------------');
     });
@@ -34,9 +38,9 @@ function PrintRegistry(nameRegistry) {
 // For testing this function generates random 20 byte strings
 function generateRandomAddress() {
     var text = "";
-    var possible = "0123456789abcde";
+    var possible = "0123456789abcde0123456789abcde0123456789abcde0123456789abcde";
 
-    for (var i = 0; i < 20; i++)
+    for (var i = 0; i < 40; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return '0x'+text;
