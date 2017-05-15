9997720e8bcd5b9427c64c3845cb3da8edb65142
rajrin@yahoo.com <raj@acloudfan.com>
2017-05-14 21:55:38 -0400

    added User registry contract

--- contracts/UserAddressRegistry.sol
+++ contracts/UserAddressRegistry.sol
@@ -1,7 +1,45 @@
 pragma solidity ^0.4.4;
 
+/**
+ * Implements the registry for address => name mapping
+ * 
+ **/
+
 contract UserAddressRegistry {
-  function UserAddressRegistry() {
-    // constructor
+
+  address   owner;
+  mapping(address => bytes32)  addressMap;
+  address[]   addresses;
+
+  modifier OwnerOnly() {
+    if(msg.sender == owner) _;
+    else throw;
+  }
+
+  function registerName(bytes32 name) returns (bool){
+    if(addressMap[msg.sender] == bytes32(0)){
+      addresses.push(msg.sender);
+    }
+    addressMap[msg.sender] = name;
+    return true;
+  }
+
+  function updateName(address given, bytes32 name) OwnerOnly returns (bool){
+    if(addressMap[given] == bytes32(0)) return false;
+    addressMap[given] = name;
+    return true;
+  }
+
+  function  count() constant returns (uint){
+    return addresses.length;
+  }
+  
+  function  getNameByIndex(uint index) constant returns (bytes32){
+    address addr = addresses[index];
+    return addressMap[addr];
+  }
+
+  function  getNameByAddress(address addr) constant returns (bytes32){
+    return addressMap[addr];
   }
 }
---
+++ test/user_address_registry.js
@@ -0,0 +1,9 @@
+
+
+contract('UserAddressRegistry', function(accounts) {
+  it("should assert true", function(done) {
+    var user_address_registry = UserAddressRegistry.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
